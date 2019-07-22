4175717261158e5258bb70ef3fcfc7f7e9bc940d
prateek.khatri <prateek.khatri@go-jek.com>
2019-07-22 15:55:39 +0530

    added JUnit test file ProducerTest.java

---
+++ test/java/tech/gojek/ziggurat/Producer/ProducerTest.java
@@ -0,0 +1,24 @@
+package java.tech.gojek.ziggurat.Producer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProducerTest {
+
+    @Before
+    public void setup() {
+//        Fixtures.mountConfig();
+//        Fixtures.mountProducer();
+    }
+
+    @After
+    public void teardown() {
+//        Fixtures.unmountAll();
+    }
+
+    @Test
+    public void shouldSendStringData() {
+
+    }
+}
