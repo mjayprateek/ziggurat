4e7263b06864faf898bd388b7d541fb646a10471
prateek.khatri <prateek.khatri@go-jek.com>
2020-09-24 13:02:49 +0530

    logging metadata for points and stats channel

--- src/ziggurat/timestamp_transformer.clj
+++ src/ziggurat/timestamp_transformer.clj
@@ -3,7 +3,8 @@
             [clojure.tools.logging :as log]
             [ziggurat.sentry :refer [sentry-reporter]]
             [sentry-clj.async :as sentry]
-            [ziggurat.util.time :refer [get-current-time-in-millis get-timestamp-from-record]])
+            [ziggurat.util.time :refer [get-current-time-in-millis get-timestamp-from-record]]
+            [protobuf.impl.flatland.mapdef :as protodef])
   (:import [org.apache.kafka.streams KeyValue]
            [org.apache.kafka.streams.kstream Transformer]
            [org.apache.kafka.streams.processor TimestampExtractor ProcessorContext]))
@@ -20,6 +21,31 @@
                (get-current-time-in-millis)
                ingestion-time))))
 
+(defn deserialize-message-2
+  [message proto-class]
+  (try
+    (let [proto-klass  (protodef/mapdef proto-class)
+          loaded-proto (protodef/parse proto-klass message)
+          proto-keys   (-> proto-klass
+                           protodef/mapdef->schema
+                           :fields
+                           keys)]
+      (select-keys loaded-proto proto-keys))
+    (catch Throwable e
+      (log/error e (str "Couldn't parse the message with proto - " proto-class)))))
+
+(defn- get-key-proto-class
+  [topic]
+  (if (= topic "driver-reward-points")
+    "com.gojek.esb.driverrewards.DriverRewardPointLogKey"
+    "com.gojek.esb.driverstatistics.DriverStatsUpdatedLogKey"))
+
+(defn- get-value-proto-class
+  [topic]
+  (if (= topic "driver-reward-points")
+    "com.gojek.esb.driverrewards.DriverRewardPointsLogMessage"
+    "com.gojek.esb.driverstatistics.DriverStatsUpdatedLogMessage"))
+
 (deftype TimestampTransformer [^{:volatile-mutable true} processor-context metric-namespace oldest-processed-message-in-s additional-tags] Transformer
          (^void init [_ ^ProcessorContext context]
            (set! processor-context context))
@@ -27,10 +53,13 @@
            (let [message-time (.timestamp processor-context)
                  partition    (.partition processor-context)
                  topic        (.topic processor-context)
-                 metadata     (str "stream record metadata--> " "record-key: " record-key " record-value: " record-value " partition: " partition " topic: " topic)]
+                 metadata     (str "stream record metadata--> " "record-key: " (deserialize-message-2 record-key (get-key-proto-class topic))
+                                   " record-value: " (deserialize-message-2 record-value (get-value-proto-class topic))
+                                   " partition: " partition
+                                   " topic: " topic)]
              (log/info metadata)
              ;; HACK:
-             (sentry/report-error sentry-reporter (Exception. "this exception is made for logging the stream metadata") metadata)
+             ;(sentry/report-error sentry-reporter (Exception. "this exception is made for logging the stream metadata") metadata)
              (when (message-to-process? message-time oldest-processed-message-in-s)
                (calculate-and-report-kafka-delay metric-namespace message-time additional-tags)
                (KeyValue/pair record-key record-value))))
